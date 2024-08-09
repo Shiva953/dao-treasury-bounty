ab5c700baa75a28b12741cb9309a7e07719072a3
Shiva953 <b22070@students.iitmandi.ac.in>
2024-08-09 19:04:18 +0530

    removed example

--- src/index.js
+++ src/index.js
@@ -179,9 +179,9 @@ async function retryWithBackoff(fn, maxRetries = 5, initialDelay = 1000) {
 }
 
 //example usage
-const programId = new PublicKey('GovER5Lthms3bLBqWub97yVrMmEogzX7xNjdXpPPCVZw');
-const realms = await getRealms(connection, programId);
-getTVL(realms)
+// const programId = new PublicKey('GovER5Lthms3bLBqWub97yVrMmEogzX7xNjdXpPPCVZw');
+// const realms = await getRealms(connection, programId);
+// getTVL(realms)
 
 //exposing the endpoint
 app.get('/tvl/dao/:daoGovernanceId', async (req, res) => {
